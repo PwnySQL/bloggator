adbd5864d48317b347de174e9beff0ef0bb011e9
PwnySQL <37075657+PwnySQL@users.noreply.github.com>
2026-02-04 08:34:36 +0100

    ch3l3: add command to list known feeds

---
+++ handler_feeds.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerFeeds(s *state, cmd command) error {
+	if len(cmd.arguments) != 0 {
+		return fmt.Errorf("%s does not expect arguments", cmd.name)
+	}
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return err
+	}
+	if len(feeds) == 0 {
+		fmt.Println("No feeds available")
+	}
+
+	for _, feed := range feeds {
+		str := fmt.Sprintf("* %s:\n", feed.Name)
+		str += fmt.Sprintf("    %s\n", feed.Url)
+		if !feed.UserID.Valid {
+			return fmt.Errorf("Invalid user id")
+		} else {
+			user, err := s.db.GetUserByID(context.Background(), feed.UserID.UUID)
+			if err != nil {
+				return err
+			}
+			str += fmt.Sprintf("    added by: %s\n", user.Name)
+			fmt.Println(str)
+		}
+	}
+	return nil
+}
